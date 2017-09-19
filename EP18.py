b4ad9358514b5df11f4963c858b9be7df4c1674a
马骏 <joshuam@foxmail.com>
2017-09-19 17:11:32 +0800

    第18题

--- EP18.py
+++ EP18.py
@@ -2,13 +2,19 @@ import fileinput
 
 
 def main():
-    nums = []
-    summary = 0
+    nums, bits, sums = [[], [], []]
     for line in fileinput.input('EP18data.txt'):
         nums.append(list(map(int, (line[:-1].split(' ')))))
-    for j in range(2**(len(nums[-1]) - 1)):
-        print(j, end=' ')
-    print(summary)
+    for i in range(2**(len(nums[-1]) - 1)):
+        temp = 0
+        summary = nums[0][temp]
+        bits = get_bits(i)
+        for (item, index) in zip(bits, range(1, len(nums[-1]))):
+            temp += item
+            summary += nums[index][temp]
+        sums.append(summary)
+    print(max(sums))
+
 
 def get_bits(num, size=3):
     mark = 1 << size
@@ -17,6 +23,7 @@ def get_bits(num, size=3):
         mark >>= 1
         bits.append((mark & num) >> (size - 1 - i))
     return bits
+
 
 if __name__ == '__main__':
     main()
